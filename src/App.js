a95baade145c670c55d1303c6605d352c55182ab
Bishal1998 <Bishalpaudel1998@gmail.com>
2022-04-21 11:20:51 +0545

    key features __final

--- src/App.js
+++ src/App.js
@@ -11,12 +11,41 @@ function App() {
   const [board, setBoard] = useState(Words);
   const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
 
+  const onSelect = () => {
+    if (currAttempt.letterPos !== 5) return;
+    setCurrAttempt((init) => {
+      return ({ attempt: init.attempt + 1, letterPos: 0 })
+    })
+  }
+
+  const onDelete = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt((init) => {
+      return ({ ...init, letterPos: init.letterPos + 1 })
+    }
+
+    )
+  }
+
+  const onEnter = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = '';
+    setBoard(newBoard);
+    setCurrAttempt((init) => {
+      return ({ ...init, letterPos: init.letterPos - 1 })
+    })
+  }
+
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt }}>
+      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt, onSelect, onDelete, onEnter }}>
         <div className="game">
           <Board />
           <Keyboard />
